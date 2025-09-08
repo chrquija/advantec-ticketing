14490532531d25127052989a287c90cbacf8e635
Christopher Quijano <cquijano@advantec-usa.com>
2025-09-07 23:58:12 -0700

    Update

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import streamlit as st
 
-st.title("ATicket")
+st.title("ATIX")
 st.subheader("Enterprise Ticketing Solution from ADVANTEC")
 
-with st.expander("Learn More About ATicket"):
+with st.expander("Learn More About ATIX"):
     st.write("""
-    ATicket is your all-in-one solution for managing service requests.
+    Need something done? Just drop a ticket! 
 
     **What you can do:**
     - Create, assign, and track **support tickets** within the ENTIRE ADVANTEC network.
     - Attach files, screenshots, and other information to each ticket.
-    - Submit work requests and charge them to a project upon approval from Project Manager.
+    - Submit work requests and charge them to a project **upon approval from Project Manager.**
     - Receive real-time notifications in Teams when your tickets are approved, updated, or completed. 
 
-    **Why choose DataDesk:**
+    **Why choose ATIX?:**
     - Secure and Reliable: Restricted access to only ADVANTEC employees.
-    - 
+    - Professional Data Engineering on demand:
+        - AI Consulting and Implementation Services
+        - Track Performance with KPI Dashboards
+        - Forecasting and Predictive Analytics
+        - Graphic Data Visualization
+        - Finding and Compiling datasets 
+        - Corridor Analysis for Transportaion Studies 
+        
+        
     """)
